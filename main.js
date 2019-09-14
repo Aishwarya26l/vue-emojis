b82166bb0c11273b1d25136b20b5539500a01b7e
Chris Boesch <ChrisBoesch@nus.edu.sg>
2019-09-14 16:04:44 +0800

    Proving directions for the current problem

--- main.js
+++ main.js
@@ -3,7 +3,20 @@ var app = new Vue({
   data: {
     title: "Learn Emojis",
     problems: [1, 2, 3, 4, 5],
-    currentProblem: 1
+    currentProblem: 1,
+    directions: {
+      "1":
+        "Use the Face With Tears of Joy 😂 emoji in place of the word crying.",
+      "2": ""
+    },
+    givens: {
+      "1": "I am crying laughing.",
+      "2": ""
+    },
+    solutions: {
+      "1": "I am 😂 laughing.",
+      "2": ""
+    }
   },
   methods: {}
 });
