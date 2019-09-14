930e912ed7e5928200ddc12bd52018b0b411fdef
Chris Boesch <ChrisBoesch@nus.edu.sg>
2019-09-14 15:47:36 +0800

    Bolding current problem

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: "#app",
   data: {
     title: "Learn Emojis",
-    problems: [1, 2, 3, 4, 5]
+    problems: [1, 2, 3, 4, 5],
+    currentProblem: 1
   },
   methods: {}
 });
