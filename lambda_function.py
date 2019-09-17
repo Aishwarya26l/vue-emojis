8eaf8dce00cc2368d3a61af9be2474b1b690aa8f
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-09-17 11:26:55 +0800

    Added additional routes handling

--- lambda_function.py
+++ lambda_function.py
@@ -1,37 +1,38 @@
 import json
 """
   This function assumes that index.html will try to load main.js
-  from ?file=main.js. You can modify your index.html script tag to be: 
-  <script src="?file=main.js"></script>
+  from /default/[REPLACE_WITH_LAMBDA_FUNCTION_NAME]/main.js. 
+  You can modify your index.html script tag to be: 
+  <script src="/default/[REPLACE_WITH_LAMBDA_FUNCTION_NAME]/main.js"></script>
+  
+  Example - 
+    Lambda function name - vueEmoji
+    <script src="/default/vueEmoji/main.js"></script>
 """
 def lambda_handler(event, context):
-    file = ""
-    qsp = event.get("queryStringParameters",{})
-    if qsp: 
-        file = qsp.get("file","")
-    
-    if file=="main.js":
-        mainPage = None
+        
+    path = event.get('path', {})
+    resultPage = None
+    result = {
+        "statusCode": 200,
+        "headers": {
+        'Content-Type': 'text/html',
+        },
+        "body": resultPage
+    }
+    if path == "/vueEmojiTest/main.js":
         with open('main.js', 'r') as f:
-            mainPage = f.read()  
-        
-        result = {
-            "statusCode": 200,
-            "headers": {
-                'Content-Type': 'text/javascript;charset=UTF-8',
-            },
-            "body": mainPage
-        }
-    else:
-        indexPage = None
+            resultPage = f.read() 
+        result["headers"]["Content-Type"] = "text/javascript;charset=UTF-8"
+    elif path == "/vueEmojiTest/package.json":
+        with open('package.json', 'r') as f:
+            resultPage = f.read()
+        result["headers"]["Content-Type"] = "application/json"
+    elif path == "/vueEmojiTest/index.html":
         with open('index.html', 'r') as f:
-            indexPage = f.read()   
-        
-        result = {
-            "statusCode": 200,
-            "headers": {
-            'Content-Type': 'text/html',
-            },
-            "body": indexPage
-        }
+            resultPage = f.read()
+    else:
+        with open('index.html', 'r') as f:
+            resultPage = f.read()
+    result["body"] = resultPage
     return result
